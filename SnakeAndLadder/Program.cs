6aac1bd779df77436f4da1a3a62802cb0754fad5
Prateek Pai <paiprateek123@gmail.com>
2022-06-11 21:59:19 +0530

    Number of Times Dice was Played and Position after Every Die Role

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -13,12 +13,15 @@ namespace SnakeAndLadder
             int StartPosition = 0;
             int PlayerPosition = 0;
             int CurrentPosition = 0;
+            int DieCount = 0;
             Console.WriteLine("Snake and Ladder Game Played with Single Palyer");
             Console.WriteLine("Start Position of the Player is : " + StartPosition);
             Console.WriteLine("Player Rolls the die");
             Console.WriteLine("........................");
             while(CurrentPosition <= 100)
             {
+                Console.WriteLine("........................");
+                Console.WriteLine("Present Position of the Player is : " + CurrentPosition);
                 Random random = new Random();
                 int die = random.Next(1, 7);
                 Console.WriteLine("The die Number is : " + die);
@@ -46,14 +49,14 @@ namespace SnakeAndLadder
                         break;
                 }
                 CurrentPosition += PlayerPosition;
+                DieCount++;
 
                 if (CurrentPosition > 100)
                 {
                     CurrentPosition--;
                 }
             }
-
-            Console.WriteLine("Current Position of the Player is : " + CurrentPosition);
+            Console.WriteLine("The number of times the dice was played to win the game: "+ DieCount);
             Console.WriteLine("Player Win the Game");
         }
     }

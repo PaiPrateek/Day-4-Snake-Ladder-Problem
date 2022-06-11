4819ee9422192572086492d9ae7dd256c214b060
Prateek Pai <paiprateek123@gmail.com>
2022-06-11 16:58:00 +0530

    Player Rolls the Die to get the Number

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -13,6 +13,11 @@ namespace SnakeAndLadder
             int StartPosition = 0;
             Console.WriteLine("Snake and Ladder Game Played with Single Palyer");
             Console.WriteLine("Start Position of the Player is : " + StartPosition);
+            Console.WriteLine("Player Rolls the die");
+            Console.WriteLine("........................");
+            Random random = new Random();
+            int die = random.Next(1,7);
+            Console.WriteLine("The die Number is : "+die);
         }
     }
 }
